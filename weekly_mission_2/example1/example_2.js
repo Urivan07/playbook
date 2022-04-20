37a5c9851a1d7e9045bd1b4d53dd2f74972cb7a3
Urivan <urivan07@gmail.com>
2022-04-20 17:00:51 -0500

    se agrega ejemplo 1.4 objeto con metodos

--- weekly_mission_2/example1/example_2.js
+++ weekly_mission_2/example1/example_2.js
@@ -1,4 +1,4 @@
-console.log("2) Creacion de un objeto con propiedades");
+console.log("Ejemplo 2 Creacion de un objeto con propiedades");
 
 const explorer = {
   name: "Urivan",
@@ -7,29 +7,41 @@ const explorer = {
   stack: ["js", "node", "react", "vue"],
   blog: {
     url: "https://github.com/Urivan07",
-    posts: "5"
+    posts: "5",
   }, // esto es un objeto
 };
 
 console.log(explorer);
-console.log("Accediendo a las propiedades del objeto")
-console.log(explorer.name)
-console.log(explorer['name'])
+console.log("Accediendo a las propiedades del objeto");
+console.log(explorer.name);
+console.log(explorer["name"]);
 
 // -------------------------------
-console.log("Ejemplo 3: Objeto con metodos!")
+console.log("Ejemplo 3: Objeto con diferentes propiedades!");
 const ajolonauta = {
-    name : "Woopa",
-    mission: "Node JS",
-    sayHelloToExplorers : function(){
-        console.log("Soy el ajolonauta, que onda!")
-    },
-    tellMeMore: function() {
-        return `Ajolonauta: ${this.name}`
-    }
-}
+  name: "Woopa",
+  mission: "Node JS",
+  sayHelloToExplorers: function () {
+    console.log("Soy el ajolonauta, que onda!");
+  },
+  tellMeMore: function () {
+    return `Ajolonauta: ${this.name}`;
+  },
+};
 
-console.log("Ajolonauta:")
-console.log(ajolonauta)
-ajolonauta.sayHelloToExplorers()
-console.log(ajolonauta.tellMeMore())
+console.log("Ajolonauta:");
+console.log(ajolonauta);
+ajolonauta.sayHelloToExplorers();
+console.log(ajolonauta.tellMeMore());
+
+console.log("Ejemplo 4: Objeto con métodos");
+const pet = {
+  name: "Terry",
+  // Esta es una función que se guarda como propiedad
+  sayHello: function () {
+    // this.name hace referencia a la propiedad del objeto
+    console.log(`${this.name} te saluda en español!`);
+  },
+};
+
+pet.sayHello();
