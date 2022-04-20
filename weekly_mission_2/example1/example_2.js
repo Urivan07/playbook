899223c69052f54e40c62b54a9ad223da0aea50b
Urivan <urivan07@gmail.com>
2022-04-20 17:03:00 -0500

    Se agrega ejemplo 1.5 objeto con metodo que recibe parametros

--- weekly_mission_2/example1/example_2.js
+++ weekly_mission_2/example1/example_2.js
@@ -45,3 +45,14 @@ const pet = {
 };
 
 pet.sayHello();
+
+
+console.log("Ejemplo 5: Objeto con método que recibe parámetros");
+const myPet = {
+    name: "Woopa",
+    sayHelloToMyPet: function(yourPet){
+      console.log(`${this.name} say's hello to ${yourPet}`)
+    }
+  }
+  
+  myPet.sayHelloToMyPet("Terry")
