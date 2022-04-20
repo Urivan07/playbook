8afbde1619b2d47459acaf393ca7b353aeb31db6
Urivan <urivan07@gmail.com>
2022-04-20 16:34:31 -0500

    Agregando ejemplo 1.2 con error en propiedades

---
+++ weekly_mission_2/example1/example_2.js
@@ -0,0 +1,18 @@
+console.log("2) Creacion de un objeto con propiedades");
+
+const explorer = {
+  name: "Urivan",
+  edad: 25,
+  mission: "Node JS",
+  stack: ["js", "node", "react", "vue"],
+  blog: {
+    url: "https://github.com/Urivan07",
+    posts: "5"
+  }, // esto es un objeto
+};
+
+console.log(explorer);
+
+console.log("Accediendo a las propiedades del objeto")
+console.log(explorer.namess)
+console.log(explorer['namesss'])
